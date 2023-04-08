d82f0210e7d4ee460063abdd07bc31749a75d1d2
jlieow <52845474+jlieow@users.noreply.github.com>
2023-04-08 19:56:58 +0800

    Update SHA for windows

--- terraformxwindows.rb
+++ terraformxwindows.rb
@@ -2,7 +2,7 @@ class Terraformxwindows < Formula
   desc "Terraformx is a Terraform Manager CLI which builds on top of Terraform CLI to provide a few additional features"
   homepage "https://github.com/jlieow/terraform_manager"
   url "https://github.com/jlieow/terraform_manager/releases/download/release%2Fone_file_terraformx_exe_windows%2Fv0.0.1/terraformx.zip"
-  sha256 "655a8580b2c0e7589a4e5fc544d6de1bd144883a55242e62b10ace8d58670954"
+  sha256 "f6d1e12f69a527d0c4b2a80b452c6df18cd815587886a3bb20c623e46a55508d"
   license "GPL-3.0"
 
   def install
